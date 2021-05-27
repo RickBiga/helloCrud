e9829c7f4925cb14be934cdeb5b110321642c654
Ricardo Bigarani <rick.biga@gmail.com>
2021-05-27 20:18:40 -0300

    commit2

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-//teste
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+const port = 3000;
+
+const usuarioRicardo = {
+    id: 1,
+    Nome: 'Ricardo',
+    Idade: 38,
+};
+
+const usuarios = [usuarioRicardo];
+
+app.get('/usuarios', (req, res) => {
+    res.json(usuarios);
+});
+
+app.listen(port, () => {
+    console.log(`Server is up and running on port ${port}`);
+});
